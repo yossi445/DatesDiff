752c372bb0efdf10c0af37b38268211894a1502d
yossi <yossi445@gmail.com>
2019-02-24 21:28:12 +0200

    Initial commit

---
+++ app/src/main/java/com/example/yossi/datesdiff/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.yossi.datesdiff;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btn;
+    Date dt1 ;
+    Date dt2 ;
+
+    TextView tvStart, tvStop, tvTotal;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(this);
+
+        tvStart=findViewById(R.id.tvStart);
+        tvStop=findViewById(R.id.tvStop);
+        tvTotal=findViewById(R.id.tvTotal);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Button b = (Button) v;
+        if (b.getText().toString().equals("start")){
+            dt1 = new Date();
+
+            dt1.getTime();
+            tvStart.setText(String.valueOf(dt1.toString()));
+
+            b.setText("stop");
+        }
+        else if (b.getText().toString().equals("stop")){
+
+            dt2 = new Date();
+            dt2.getTime();
+
+            tvStop.setText(String.valueOf(dt2.toString()));
+
+            long diff = dt2.getTime() - dt1.getTime();
+            long diffMinutes = diff / (60 * 1000) % 60;
+
+            tvTotal.setText(String.valueOf(diffMinutes));
+
+            //b.setText("st");
+            btn.setVisibility(View.INVISIBLE);
+
+
+
+
+        }
+
+
+
+
+
+
+    }
+}
